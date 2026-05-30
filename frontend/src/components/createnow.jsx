a44b3753a597c0b22956fe597845a0745e498942
FaizanKhan-py <fk15233@gmail.com>
2026-05-30 15:34:41 +0500

    added imggen api

--- frontend/src/components/createnow.jsx
+++ frontend/src/components/createnow.jsx
@@ -14,7 +14,7 @@ function Creatnow() {
     setImageUrl("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/imagegeneration", {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}api/image/generate`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: choice.toLowerCase() })

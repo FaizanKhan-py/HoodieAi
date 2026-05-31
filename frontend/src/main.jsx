4943d939ffb781b4b70943c9e37271c2bbc36f70
FaizanKhan-py <fk15233@gmail.com>
2026-05-31 22:10:52 +0500

    feat: apply Cormorant Garamond font to navbar and headings

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import "@fontsource/cormorant-garamond";
-import "@fontsource/inter";
-import "@fontsource/poppins";
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
